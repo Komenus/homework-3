62aff1b0166ed8e00e385cb7f6a810bbf09ea6d2
dmitriyzheleznyak <komenus@gmail.com>
2022-08-19 10:57:31 +0300

    Third task add

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+double cube = 1;
+int count = 1;
+while(count <= N )
+{
+    cube = Math.Pow(count, 3);
+    Console.WriteLine($"Куб числа {count} : {cube}");
+    count ++;
+}
